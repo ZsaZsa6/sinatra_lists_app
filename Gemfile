b1e3451444b6fcbd530743cc49d7e93115b264ad
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-24 03:10:19 +0000

    gemfile updated, sessions controller completed

--- Gemfile
+++ Gemfile
@@ -1,18 +1,13 @@
 source 'http://rubygems.org'
-gem "sinatra", "~> 2.0"
 
-gem "shotgun", "~> 0.9.2"
-
-gem "sinatra-activerecord", "~> 2.0"
-
-gem "activerecord", "~> 6.0"
-
-gem "rake", "~> 13.0"
-
-gem "sqlite3", "~> 1.4"
-
-gem "pry", "~> 0.13.1"
-
-gem "require_all", "~> 3.0"
-
-gem "bcrypt", "~> 3.1"
+gem 'sinatra'
+gem 'activerecord', '~> 6.0.0', :require => 'active_record'
+gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'rake'
+gem 'require_all'
+gem 'sqlite3'
+gem 'thin'
+gem 'shotgun'
+gem 'pry'
+gem 'bcrypt'
+gem "tux"
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,42 @@
+class SessionsController < ApplicationController
+
+    get '/signup' do
+        erb :'sessions/signup'
+    end
+
+    get '/login' do
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        u = User.find_by(username: params[:username], email: params[:email])
+        if u && u.authenticate(params[:password])
+
+            session[:user_id] = u.id
+            redirect '/lists'
+        else
+
+            @err = "Invalid Credentials"
+            erb :'sessions/login'
+        end
+    end
+
+
+    post '/signup' do
+        @u = User.new(name: params[:name], email: params[:email], password: params[:password], username: params[:username])
+        if @u.save
+
+            session[:user_id] = @u.id
+            redirect "/lists"
+        else
+
+            erb :'sessions/signup'
+        end
+    end
+
+    delete '/logout' do
+        session.clear
+        redirect '/login'
+    end
+
+end
